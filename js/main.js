df09c937a9bb829721b8d76561f5711ac856bec5
DaDoDev7 <dario.plastina@gmail.com>
2023-06-27 12:58:07 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -39,24 +39,15 @@ window.addEventListener("scroll", function(e) {
 
 var loaderShown = document.cookie.includes('loaderShown=true');
 
-if (!loaderShown) {
-  window.addEventListener('load', function() {
-    var loader = document.querySelector('.loader_wrapper');
-    setTimeout(function() {
-      loader.classList.add('fade');
-      setTimeout(function() {
-        loader.style.display = 'none';
-      }, 1000); 
-    }, 3600);
-
-    // Imposta un cookie per indicare che il loader è stato visualizzato
-    document.cookie = 'loaderShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT';
-  });
-} else {
-  // Il loader è già stato visualizzato precedentemente, quindi nascondilo immediatamente
+window.addEventListener('load', function() {
   var loader = document.querySelector('.loader_wrapper');
-  loader.style.display = 'none';
-}
+  setTimeout(function() {
+    loader.classList.add('fade');
+    setTimeout(function() {
+      loader.style.display = 'none';
+    }, 1000); 
+  }, 3600);
+});
 
 //nav class  darken
 
